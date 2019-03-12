2cb43696dce4b53dd9e5507e49c5ab91fa894ac6
Joe Muro <jmuro0321@gmail.com>
2019-03-12 14:28:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,7 +26,6 @@ def valid_move?(board, index)
   return index.between?(0, board.length) && !position_taken?(board, index)
 end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   position = board[index]
   
