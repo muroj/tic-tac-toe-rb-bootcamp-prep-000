ef294b465d36b1d3f5e01dda8c08e8e3883c8aed
Joe Muro <jmuro0321@gmail.com>
2019-03-12 14:12:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,6 +52,23 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  turns = 0
+  
+  board.each do |position|
+    if position.eql?("X") || position.eql?("O")
+      turns += 1
+    end
+  end
+  
+  return turns
+end 
+
+def current_player(board)
+  # Assume player X goes first
+  return turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0, 1, 2], # Row wins
